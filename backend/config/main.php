27c852703d899097f205d3f204994b756919406b
Korol Dima <korol_dima@list.ru>
2015-08-24 11:56:33 +0300

    Editor

--- backend/config/main.php
+++ backend/config/main.php
@@ -91,5 +91,48 @@ return [
             'errorAction' => 'site/error',
         ],
     ],
+    'controllerMap' => [
+        'elfinder' => [
+            'class' => 'mihaildev\elfinder\Controller',
+            'access' => ['@', '?'],
+//глобальный доступ к фаил менеджеру @ - для авторизорованных , ? - для гостей , чтоб открыть всем ['@', '?']
+            'disabledCommands' => ['netmount'],
+//отключение ненужных команд https://github.com/Studio-42/elFinder/wiki/Client-con..
+            'roots' => [
+                [
+                    'baseUrl' => '',
+                    'basePath' => '@frontend/web',
+                    'path' => 'image/upload',
+                    'name' => 'Изображения',
+
+//'uploadAllow' => ['image/png', 'image/jpeg', 'image/gif'],
+                ],
+// [
+// 'class' => 'mihaildev\elfinder\UserPath',
+// 'path' => 'files/user_{id}',
+// 'name' => 'My Documents'
+// ],
+// [
+// 'path' => 'image/some',
+// 'name' => ['category' => 'my', 'message' => 'Some Name'] //перевод Yii::t($category, $message)
+// ],
+// [
+// 'path' => 'image/some',
+// 'name' => ['category' => 'my', 'message' => 'Some Name'], // Yii::t($category, $message)
+// 'access' => ['read' => '*', 'write' => 'UserFilesAccess']
+// // * - для всех, иначе проверка доступа в даааном примере все могут видет а редактировать могут пользователи только с правами UserFilesAccess
+// ]
+            ],
+            'watermark' => [
+                'source' => __DIR__ . '/logo.png', // Path to Water mark image
+                'marginRight' => 5, // Margin right pixel
+                'marginBottom' => 5, // Margin bottom pixel
+                'quality' => 95, // JPEG image save quality
+                'transparency' => 70, // Water mark image transparency ( other than PNG )
+                'targetType' => IMG_GIF | IMG_JPG | IMG_PNG | IMG_WBMP, // Target image formats ( bit-field )
+                'targetMinPixel' => 200 // Target image minimum pixel size
+            ]
+        ]
+    ],
     'params' => $params,
 ];
