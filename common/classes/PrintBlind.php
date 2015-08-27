e98ff3999ab42fc42cc125b36f39cbf0b46c9e0a
morozovigor <perffectgame@gmail.com>
2015-08-27 11:43:50 +0300

    проверка на отсутствие элементов

--- common/classes/PrintBlind.php
+++ common/classes/PrintBlind.php
@@ -39,30 +39,42 @@ class PrintBlind
         //достаем все жалюзи текущей категории
         $blinds = BlindCatid::find()->where(['id_cat' => $id])->offset($num*2)->limit(2)->all();
         //получаем описание объекта жалюзи
-        $blindObj1 = Blind::find()->where(['id' => $blinds[0]->id_blind])->one();
-        if(isset($blinds[1])){
-            $blindObj2 = Blind::find()->where(['id' => $blinds[1]->id_blind])->one();
-            $blindImgs2 = BlindImg::find()->where(['id_blind' => $blindObj2->id])->all();
-        }
-        //получаем картинки для жалюзи
-        $blindImgs1 = BlindImg::find()->where(['id_blind' => $blindObj1->id])->all();
-        //start of page
-        $html .= '<div class="page">';
+        if(isset($blinds[0]) || isset($blinds[1])){
+            if(isset($blinds[0])) {
+                $blindObj1 = Blind::find()->where(['id' => $blinds[0]->id_blind])->one();
+                //получаем картинки для жалюзи
+                $blindImgs1 = BlindImg::find()->where(['id_blind' => $blindObj1->id])->all();
+            }
+            if(isset($blinds[1])){
+                $blindObj2 = Blind::find()->where(['id' => $blinds[1]->id_blind])->one();
+                $blindImgs2 = BlindImg::find()->where(['id_blind' => $blindObj2->id])->all();
+            }
 
-        if($realNum > 1){
-            $html .= '<div class="title">
+            //start of page
+            $html .= '<div class="page">';
+
+            if($realNum > 1){
+                $html .= '<div class="title">
                         <span>Страница '.$realNum.'  </span>
                         <a href="#" class="hidepage">Скрыть</a>
                     </div>';
+            }
+
+            if(isset($blindObj1) && !empty($blindObj1)){
+                $html .= self::getItem($blindObj1,$blindImgs1);
+            }
+
+            if(isset($blindObj2) && !empty($blindObj2)){
+                $html .= self::getItem($blindObj2,$blindImgs2);
+            }
+
+            //end of page
+            $html .= '</div>';
         }
 
-        $html .= self::getItem($blindObj1,$blindImgs1);
 
-        if(isset($blindObj2) && !empty($blindObj2)){
-            $html .= self::getItem($blindObj2,$blindImgs2);
-        }
-        //end of page
-        $html .= '</div>';
+
+
         return $html;
     }
 
